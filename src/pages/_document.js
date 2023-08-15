6afc628cd2093a71321cd2fc07ec0351b007a43d
neiht <thienphan9612@gmail.com>
2023-08-15 08:34:16 +0700

    new

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+            <meta property='og:type' content={'web'} />
+            <meta property='og:site_name' content={'mKara'} />
+            <meta property='og:description' content={'karaoke'} />
+            <meta property='og:title' content={'mKara - Karaoke'} />
+            <meta name='image' property='og:image' content={'/mango.png'} />
+        </Head>
       <body>
         <Main />
         <NextScript />
