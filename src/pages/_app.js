08730f2df1676f00f15c83469b34394f4b8b2a45
neiht <thienphan9612@gmail.com>
2023-08-23 10:21:25 +0700

    new

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -31,13 +31,13 @@ useEffect(()=>{
   return(
       <AuthProvider>
     <App safeAreas theme={ios ? "ios": "material"}>
-      <Head>
-        <title>Khoai Lang Châu Thành</title>
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover"
-        />
-      </Head>
+      {/*<Head>*/}
+      {/*  <title>Khoai Lang Châu Thành</title>*/}
+      {/*  <meta*/}
+      {/*      name="viewport"*/}
+      {/*      content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover"*/}
+      {/*  />*/}
+      {/*</Head>*/}
       <Component {...pageProps} />
       <Nav/>
     </App>
